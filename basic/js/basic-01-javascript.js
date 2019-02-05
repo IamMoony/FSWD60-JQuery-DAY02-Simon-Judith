2e07e19ca834388ad87c79597dc31e1b866c03ee
Judith <judithmarialang@gmail.com>
2019-02-05 10:45:19 +0100

    adds img for basic 2

--- basic/js/basic-01-javascript.js
+++ basic/js/basic-01-javascript.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
 
-$("div.box").on('click', function() {
-	$text = $(this).attr("text");
-	$("#click_result").text($text);
+// $("div.box").on('click', function() {
+// 	$text = $(this).attr("text");
+// 	$("#click_result").text($text);
+// });
+
+$("div.box:eq(1)").on('click', function(){
+	$childBox = $(this).children();
+	$($childBox).hide(1000,"linear");
+
 });
+
 
 });
