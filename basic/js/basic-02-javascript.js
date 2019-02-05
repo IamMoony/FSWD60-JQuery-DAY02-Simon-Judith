cf8d398b5bd509370734559fcb3cf8e192ed9bc4
IamMoony <s.blaha@live.at>
2019-02-05 11:26:12 +0100

    finish basic exercise 2

---
+++ basic/js/basic-02-javascript.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$("img").on('click', function() {
+		// $(this).hide(1500);
+		 // $(this).fadeOut(1500).slideDown(1000).slideUp(1000).fadeIn(1000);
+		// $(this).slideUp(1500);
+		// $(this).css({"width": "0px"});
+		// $(this).css("visibility", "hidden");
+		// $(this).remove();
+	});
+});
