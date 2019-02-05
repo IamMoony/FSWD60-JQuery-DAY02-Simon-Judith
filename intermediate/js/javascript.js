035e88c8cff8e39641c09b2bb023c7192d5f3acd
IamMoony <s.blaha@live.at>
2019-02-05 14:09:29 +0100

    change javascript js to jquery syntax

--- intermediate/js/javascript.js
+++ intermediate/js/javascript.js
@@ -3,14 +3,18 @@
 	// 	function all_items (data){
 	// 		cart_items.push(data)
 	// 	}
-
+	$(document).ready(function(){
 		for (var i=0; i<data.length; i++){
+		   var items = data[i];
 
-		  var items = data[i];
+			$("#items").append("<div class='item_Container' name='"+items.itemName+""<br>"'price='"+items.itemPrice+"'value="1"><img src='" + items.itemPic + "'>" + "<br><p>" + items.itemName + "<br>" + items.itemPrice+ "</p></div>");
 
-			document.getElementById("items").innerHTML += "<div class='item_Container' name='"items.itemPic"'><img src='" + items.itemPic + "'>" + "<br><p>" + items.itemName + "<br>" + items.itemPrice+ "</p></div>";
+		}
 
-		}	
+		var item = $("#items").attr("id");
+		$("#items").append(item)
+	});
+		
 
 		// document.addEventListener('load',function(){all_items(items[i])}, false);
 		// $("body").on("load",function(){
